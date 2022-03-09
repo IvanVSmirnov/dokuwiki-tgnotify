20b1e0f364462d2385caee8df260bf7880867a80
Ivan Smirnov <ivsmir@gmail.com>
2022-03-10 00:28:11 +0300

    Imprevement: russian translation imprevements

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -1,5 +1,5 @@
 <?php
-$lang["enable"] = 'Включить оповещения через Telegram';
+$lang["enable"] = 'Включить Telegram оповещения ';
 $lang["token"] = 'Токен telegram бота';
-$lang["chatid"] = 'Идентификатор чата telegram (chat ID) для рассылки сообщений';
+$lang["chatid"] = 'Идентификатор чата telegram (chat ID)';
 $lang["silent"] = 'Отпралять "тихие" сообщения, отключить уведомления получателям';
