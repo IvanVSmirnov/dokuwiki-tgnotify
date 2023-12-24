e5892dd6633a8ecce2dea26ec20cda6711f6cee0
Ivan Smirnov <ivsmir@gmail.com>
2023-12-24 16:26:11 +0300

    New language added: Ukrainian (UA)

---
+++ lang/ua/lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang["pagename"] = '__Зміна на сторінці__: [%s](%s)';
+$lang["sizechange"] = '__Зміна розміру сторінки__: `%+d байт`';
+$lang["page-added"] = '*Нова сторінка*';
+$lang["page-deleted"] = '*Сторінка видалена*';
+$lang["page-modified"] = '*Сторінка змінена*';
+$lang["page-modified-minor"] = '*Незначна зміна на сторінці*';
+$lang["page-reverted"] = '*Відновлення попередньої версії сторінки*';
+$lang["username"] = '__Користувач, що вніс зміну__: `%s`';
+$lang["useraddr"] = '__Адреса__: `%s`';
+$lang["unknown"] = 'невизначен';
---
+++ lang/ua/settings.php
@@ -0,0 +1,7 @@
+<?php
+$lang["enable"] = 'Увімкнути Telegram оповіщення';
+$lang["debug"] = 'Увімкнути режим налагодження плагіна';
+$lang["token"] = 'Токен telegram бота';
+$lang["chatid"] = 'Ідентифікатор чата telegram (chat ID)';
+$lang["showuser"] = 'Показувати ім"я користувача';
+$lang["showaddr"] = 'Показувати IP адресу';
